bbe21494dc95b3e8deba33b225e2c38a98a28eb6
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-10-03 03:04:31 -0400

    Add documentations.

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# An implementation of Fraud Eagle algorithm
+# Fraud Eagle Algorithm
 [![GPLv3](https://img.shields.io/badge/license-GPLv3-blue.svg)](https://www.gnu.org/copyleft/gpl.html)
 [![Build Status](https://travis-ci.org/rgmining/fraud-eagle.svg?branch=master)](https://travis-ci.org/rgmining/fraud-eagle)
+[![wercker status](https://app.wercker.com/status/fb1061f0043991bf7609a198a96acbaf/s/master "wercker status")](https://app.wercker.com/project/byKey/fb1061f0043991bf7609a198a96acbaf)
 [![Code Climate](https://codeclimate.com/github/rgmining/fraud-eagle/badges/gpa.svg)](https://codeclimate.com/github/rgmining/fraud-eagle)
 [![Release](https://img.shields.io/badge/release-0.9.0-brightgreen.svg)](https://github.com/rgmining/fraud-eagle/releases/tag/v0.9.0)
 
+This package provides an implementation of Fraud Eagle algorithm.
 This algorithm has been introduced by Leman Akoglu, *et al.* in
 [AAAI 2013](https://www.aaai.org/ocs/index.php/ICWSM/ICWSM13/paper/viewFile/5981/6338)
 
--- fraud_eagle/likelihood.py
+++ fraud_eagle/likelihood.py
@@ -1,5 +1,5 @@
 #
-# setup.py
+# likelihood.py
 #
 # Copyright (c) 2016 Junpei Kawamoto
 #
@@ -58,15 +58,18 @@ def psi(user, product, review, epsilon):
         User: Fraud, 1 - 2 :math:`\\epsilon`, 2 :math:`\\epsilon`
 
     Args:
-      user: user label which must be one of the
-        {:data:`HONEST<fraud_eagle.constants.HONEST>`,
-        :data:`FRAUD<fraud_eagle.constants.FRAUD>`},
-      product: product label which must be one of the
-        {:data:`GOOD<fraud_eagle.constants.GOOD>`,
-        :data:`BAD<fraud_eagle.constants.BAD>`},
-      review: review label which must be one of the
-        {:data:`PLUS<fraud_eagle.constants.PLUS>`,
-        :data:`MINUS<fraud_eagle.constants.MINUS>`},
+      user: user label which must be one of the { \
+        :data:`HONEST<fraud_eagle.constants.HONEST>`, \
+        :data:`FRAUD<fraud_eagle.constants.FRAUD>`}.
+
+      product: product label which must be one of the \
+        {:data:`GOOD<fraud_eagle.constants.GOOD>`, \
+        :data:`BAD<fraud_eagle.constants.BAD>`}.
+
+      review: review label which must be one of the \
+        {:data:`PLUS<fraud_eagle.constants.PLUS>`, \
+        :data:`MINUS<fraud_eagle.constants.MINUS>`}.
+
       epsilon: a float parameter in :math:`[0,1]`.
 
     Returns:
